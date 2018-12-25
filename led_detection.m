0050803ca5f7ead3efe3a6b415e34a913f6e3f44
Nico Alavi <burgshrimps@mbp.local>
2018-12-26 00:58:15 +0100

    updated file input + comments

--- led_detection.m
+++ led_detection.m
@@ -1,4 +1,12 @@
-clear;
+% Automatic detection of LED light in a video
+%
+% Nico Alavi, 26. Dec 2018, <nico.alavi aT fu-berlin.de>
+% Leibniz-Institute for Neurobiology, Magdeburg
+% Department Functional Architecture of Memory
+%
+% For further information see 'README.md'.
+
+clear
 
 %% Set parameters
 filepath = input('Enter path to files: ');
@@ -9,22 +17,31 @@ sensitivity = 0.985; % for imfindcircles(), higher numbers detect more circles
 edgethreshold = 0.7; % for imfindcircles(), higher number detects fewer circles with weak edges
 step_margin = 0.05; % tolerance for skipping frames regarding the difference between button and cam
 radius_range = [9 12]; % for imfindcircles(), specifies how big the circles are 
-pxval_thr = 200;
-pxnum_thr = 12;
+pxval_thr = 200; % for brightness detection, threshold for pixel values
+pxnum_thr = 12; % for brightness detection, number of pixels which have to reach that threshold
 
 %% Open and read digialIO files
 digitalIO_info = strcat(filepath,'/','*_digitalIO.txt'); % get frame timestamp.txt file in current directory
 digitalIO_struct = dir(digitalIO_info);
 digitalIO_name = digitalIO_struct.name;
 digitalIO_file = strcat(filepath, '/', digitalIO_name);
-%comma2point_overwrite(digitalIO_file); % replace commas with dots for better handling
-[time, type, value] = textread(digitalIO_file, '%f %s %u', 'headerlines', 1, 'endofline', '\r\n'); % read data from file
+comma2point_overwrite(digitalIO_file); % replace commas with dots for better handling
+[dtime, dtype, dvalue] = textread(digitalIO_file, '%f %s %u', 'headerlines', 1, 'endofline', '\r\n'); % read data from file, skip 5 first lines since video recording starts after that
+
+% value explanation:
+% 4 -> frame captured
+% 2 -> button pressed
+% 6 (4+2) -> frame captured and button pressed at the same time
+% 0 -> event stopped
+
+% remove the event stopped (value == 0) entries since we only need to
+% consider beginning of event (e.g. start recording frame)
+dtype = dtype(dvalue == 'INPUT');
+dtime = dtime(dvalue ~= 0);
+dvalue = dvalue(dvalue ~= 0);
 
 % Get timestamps of button presses
-key_ind = find(strcmp(type,'KEY')); % we only want tpye == INPUT
-button_ind = union(find(value == 2), find(value == 6));
-% button_ind = button_ind(button_ind ~= key_ind);
-button_all = time(button_ind);
+button_all = dtime(dvalue == 6);
 
 % Filter button timestamps since we only need the first frame in which it was
 % pressed every time and digitalIO contains every frame the button remained
@@ -37,44 +54,43 @@ if ~isempty(button)
 end
 
 
-%% Open and read CVB timestamp files
+%% Open and read CVB timestamp file
 cvb_info = strcat(filepath, '/', '*_CVB_TS.txt'); % get cvb timestamp .txt file in current directory
 cvb_struct = dir(cvb_info);
 cvb_name = cvb_struct.name;
 cvb_file = strcat(filepath, '/', cvb_name);
-
 cvb_fid = fopen(cvb_file);
 cvb_raw = textscan(cvb_fid, '%s', 'Delimiter', '\n');
 
 % Insert missing zeros
-cvb_raw = cvb_raw{1}(:);
-zeros_idx = find(cellfun(@(x) length(x), cvb_raw) < 23);
-cvb_raw(zeros_idx) = cellfun(@(x) insert_zeros(x), cvb_raw(zeros_idx));
+cvb_raw = cvb_raw{1}(:); % cell formatting
+zeros_idx = find(cellfun(@(x) length(x), cvb_raw) < 23); % find those lines which dont exceed a certain length (23). Those are the lines which are missing a zero on the tens decimal position
+cvb_raw(zeros_idx) = cellfun(@(x) insert_zeros(x), cvb_raw(zeros_idx)); % correct the lines in question
 
-% Convert datetime timestamp to timestamp format of camera
-start_ts = str2date(cvb_raw{1});
-cvb = etime(str2date(cvb_raw(:)),start_ts);
-
-% cvb = sort(cvb); % in case somewhere on the way order of timestamps getting mixed
+% Convert datetime timestamp to timestamp format of axona (starting with zero increasing in seconds)
+formatIn = 'yy/mm/dd HH:MM:SS.FFF';
+start_ts = datevec(cvb_raw{1}, formatIn);
+cvb = etime(datevec(cvb_raw(:), formatIn), start_ts);
+cvb = sort(cvb);
 
 if ~isempty(cvb)
     disp('Successfully read cvb timestamps from .txt file!')
 end
 
-%% Open and read video from .avi file
-avi_info = strcat(filepath, '/', '*.avi');
-avi_struct = dir(avi_info);
-avi_name = avi_struct.name;
-avi_file = strcat(filepath, '/', avi_name);
-[out_path, out_name, out_ext] = fileparts(avi_file);
+%% Open and read video from video file
+video_info = strcat(filepath, '/', '*.mp4');
+video_struct = dir(video_info);
+video_name = video_struct.name;
+video_file = strcat(filepath, '/', video_name);
+[out_path, out_name, out_ext] = fileparts(video_file);
 
-video = VideoReader(avi_file);
+video = VideoReader(video_file);
 i = start_time * FPS;
 if ~isempty(video)
-    disp('Successfully read video from .avi file!')
+    disp('Successfully read video from file!')
 end
 
-%% Specify ROI
+%% Specify ROI (region of interest)
 disp('Displaying preview...')
 first_frame = read(video,i);
 imshow(first_frame);
@@ -82,16 +98,18 @@ imshow(first_frame);
 LEDx = round(LEDx); % round input coordinates
 LEDy = round(LEDy);
 
-ORIx = LEDx - ROI_size;
-ORIy = LEDy - ROI_size;
-width = ROI_size * 2;
-height = ROI_size * 2;
-rectangle('Position',[ORIx ORIy width height], 'EdgeColor', 'y');
+ORIx = LEDx - ROI_size; % x coordinate of origin of ROI
+ORIy = LEDy - ROI_size; % y coordinate of origin of ROI
+width = ROI_size * 2; % width of ROI
+height = ROI_size * 2; % heigth of ROI
+rectangle('Position',[ORIx ORIy width height], 'EdgeColor', 'y'); % visualize ROI on screen
 waitforbuttonpress;
 close;
 
 %% Video analysis
-% create folder to store detected frames in
+% create folder to store detected frames in, every time an LED light got
+% detected the corresponding frame gets stored as an image to check for
+% possible errors
 detection_folder = strcat(out_path, '/DetectedFrames');
 if exist(detection_folder, 'dir') ~= 7
     crtfolder = char(strcat('mkdir', {' '}, detection_folder));
@@ -105,22 +123,26 @@ video_idx = zeros(1, num_button);
 num_frames = video.NumberOfFrames;
 event_counter = 1;
 
-tic % start timer
-
 disp('Processing video...')
 while i <= num_frames
-   frame = read(video,i);
-   frame_crop = imcrop(frame, [ORIx ORIy width height]);
-   [center, radius] = imfindcircles(frame_crop, radius_range, 'Sensitivity', sensitivity, 'ObjectPolarity', 'bright', 'EdgeThreshold', edgethreshold);
-   max_px_value = max(frame_crop);
-   max_px_value = max_px_value(:,:,1);
-   num_px_over_thr = length(find(max_px_value > pxval_thr));
+   frame = read(video,i); % read in current frame
+   frame_crop = imcrop(frame, [ORIx ORIy width height]); % crop frame to ROI so less data has to be considered for the detection
+   max_px_value = max(frame_crop); % find rows of pixels with values over threshold
+   max_px_value = max_px_value(:,:,1); % image has 3 planes but since its a grayscale image they all are the same and we only have to look at one
+   num_px_over_thr = length(find(max_px_value > pxval_thr)); % find number of pixels with value over brightness threshold
    if num_px_over_thr >= pxnum_thr
+       [center, radius] = imfindcircles(frame_crop, radius_range, 'Sensitivity', sensitivity, 'ObjectPolarity', 'bright', 'EdgeThreshold', edgethreshold); % search for circular objects 
        if ~isempty(center)
+           fprintf('Detected timestamp %i/%i \n', event_counter, num_button);
+           % if bright circle (LED light) has been detected save
+           % corresponding frame index and timestamp 
            video_timestamps(event_counter) = cvb(i);
            video_idx(event_counter) = i;
-           diff = video_timestamps(event_counter) - button(event_counter);
-           fprintf('Detected timestamp %i/%i \n', event_counter, num_button);
+           
+           % look at time difference between current LED and button to have 
+           % an estimate about how much time difference will be between 
+           % between the next LED and button
+           diff = video_timestamps(event_counter) - button(event_counter); 
 
            % Save detected frame for checking
            fig = figure('visible', 'off');
@@ -163,9 +185,6 @@ fclose(fileID_idx);
 
 disp('Successfully wrote video timestamps and frame index to .txt files!')
 
-toc % end timer
-
-
 %% Functions
 function comma2point_overwrite(filespec)
     % Replaces all commas in a .txt file with dots
@@ -175,12 +194,10 @@ function comma2point_overwrite(filespec)
     file.Data(transpose(file.Data==comma)) = point;
 end 
 
-function tvec = str2date(str)
-    formatIn = 'yy/mm/dd HH:MM:SS.FFF';
-    tvec = datevec(str, formatIn);
-end
-
 function button = filterbutton(button_all)
+    % filters list of button presses to only use the timestamp in which the
+    % button was pressed first (there gotta be a 2s difference between the
+    % button presses in order to be considered unique)
     button = zeros(1,30);
     button(1) = button_all(1);
     j = 2;
@@ -193,6 +210,7 @@ function button = filterbutton(button_all)
 end
 
 function new_str = insert_zeros(str)
+    % inserts zeros in timestamp string depending on how long the string is
     str = char(str);
     new_str = strcat(str(1:20),'0',str(21:end));
     new_str = {new_str};
